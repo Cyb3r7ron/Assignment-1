e47adf9e45f96c7a7ec0e843b0ca9703d7e019f7
Cyb3r7ron <kiduskyle@gmail.com>
2022-01-06 05:35:22 -0500

    Final commit

---
+++ reverse string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This is a string reversing code written by Kidus Girma ATE/6954/11
+
+namespace reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = string.Empty;
+            string reverseWord = string.Empty;
+
+            Console.WriteLine("Enter any word");
+            word = Console.ReadLine();
+            for (int i = word.Length - 1; i >= 0; i--)
+                reverseWord += word[i];
+            Console.WriteLine("Reverse Word :-" + reverseWord);
+            Console.ReadKey();
+        }
+    }
+}
+// This is a string reversing code written by Kidus Girma ATE/6954/11
